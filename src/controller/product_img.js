0985cc88815c3efe1506e7f686a977d21cb5773f
username <byoungdo95@gmail.com>
2022-02-10 16:57:21 +0900

    psps-admin_임시 배포

--- src/controller/product_img.js
+++ src/controller/product_img.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { ADDRESS } from '../config';
 
 const instance = axios.create({
-	proxy: `${ADDRESS}`,
-	baseURL: '/product_image',
-	// baseURL: `${ADDRESS}/product_image`,
+	// proxy: `${ADDRESS}`,
+	// baseURL: '/product_image',
+	baseURL: `${ADDRESS}/product_image`,
 	withCredentials: true,
 });
 
