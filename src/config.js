a27abcf0cd9af5d50a11949e13197be48f6cd57f
username <byoungdo95@gmail.com>
2022-03-14 12:56:23 +0900

    psps-admin test

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const ADDRESS = process.env.REACT_APP_ADDRESS;
 export const IMG_ADDRESS = process.env.REACT_APP_IMG_ADDRESS;
 export const CLIENT_ADDRESS = process.env.REACT_APP_CLIENT_ADDRESS;
+
+console.log('process', process.env);
