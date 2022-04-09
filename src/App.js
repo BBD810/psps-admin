4a5e1710dde370454dbae191d46bd87c2354963a
username <byoungdo95@gmail.com>
2022-04-09 22:48:07 +0900

    사용자 인증(authentication)로직을 간단하게 수정하라

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { adminLogin } from './modules/admin';
+import * as auth from './controller/auth';
 import Background from './view/components/Common/Background';
-// import Auth from './hoc/auth';
 import LoginPage from './view/pages/LoginPage';
 import BannerPage from './view/pages/BannerPage';
 import SupplierPage from './view/pages/SupplierPage';
@@ -12,22 +14,31 @@ import UserPage from './view/pages/UserPage';
 import FaqPage from './view/pages/FaqPage';
 
 const App = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const isAuthorized = useSelector((state) => state.admin.login);
+
+	useEffect(() => {
+		auth.authCheck().then((res) => {
+			res.data.success ? dispatch(adminLogin()) : navigate('login');
+		});
+		// eslint-disable-next-line
+	}, [isAuthorized]);
+
 	return (
-		<BrowserRouter>
-			<div id='App'>
-				<Background />
-				<Routes>
-					{<Route path='/' element={<BannerPage />} />}
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/supplier' element={<SupplierPage />} />
-					<Route path='/product' element={<ProductPage />} />
-					<Route path='/product-img' element={<ProductImgPage />} />
-					<Route path='/order' element={<OrderPage />} />
-					<Route path='/user' element={<UserPage />} />
-					<Route path='/faq' element={<FaqPage />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<div id='App'>
+			<Background />
+			<Routes>
+				<Route path='/' element={<BannerPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/supplier' element={<SupplierPage />} />
+				<Route path='/product' element={<ProductPage />} />
+				<Route path='/product-img' element={<ProductImgPage />} />
+				<Route path='/order' element={<OrderPage />} />
+				<Route path='/user' element={<UserPage />} />
+				<Route path='/faq' element={<FaqPage />} />
+			</Routes>
+		</div>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './modules';
@@ -14,9 +15,11 @@ const store = createStore(rootReducer);
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
 		<Provider store={store}>
-			<GlobalStyle />
-			<GlobalFonts />
-			<App />
+			<BrowserRouter>
+				<GlobalStyle />
+				<GlobalFonts />
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</ThemeProvider>,
 	document.getElementById('root')
